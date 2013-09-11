b00c9b3d125297be5901934cff43c180192c94fa
Jeffrey Sun <jeffrey.zksun@gmail.com>
2013-09-11 11:49:46 +0800

    Enhance the update function.
    Run npm test to avoid the regression

--- test/test.js
+++ test/test.js
@@ -1,10 +1,16 @@
+/*
+How to run this test.
+1. Start the mongodb on the local machine.
+2. run npm test under the mongostream directory.   
+*/
 
 var mongostream = require("../")();
 var should = require("should");
 
 var appObject = {
 	id: new Date().toString(),
-	name: "jeffrey"		
+	name: "jeffrey",
+    age: 13
 };
 
 var string_data = 'Hello world';
@@ -18,7 +24,7 @@ describe('Mongo', function(){
 		mongostream.addSupportedCollections(["user", "storage"]);
 
 		var dboptions = {
-			host: "127.0.0.1",
+			host: "127.0.0.1", // "127.0.0.1", "10.31.149.122"
 			port: 27017,
 			name: "service"
 		};
@@ -29,31 +35,40 @@ describe('Mongo', function(){
 	});
 
 	describe('workflow', function(){
-		it('.insert should return the new inserted object', function(done){
+		it('.insert should return the new inserted object only with the passed in fields', function(done){
 			mongostream.insert('user', appObject, function(err, obj){
 				should.not.exist(err);
 				should.exist(obj);
+                obj.should.not.have.property('_id');
 				obj.should.have.property('id');
 				obj.should.have.property('name', 'jeffrey');	
+                obj.should.have.property('age', 13);                
 				
 				done();
 			});	  
 		});  
 		
-		it('.queryByID should return the new inserted object', function(done){
+		it('.queryByID should return the new inserted object only with the passed in fields', function(done){
 			mongostream.queryByID('user', appObject.id, function(err, obj){
 				should.not.exist(err);
 				should.exist(obj);
+                obj.should.not.have.property('_id');
 				obj.should.have.property('id');
 				obj.should.have.property('name', 'jeffrey');	
+                obj.should.have.property('age', 13);    
 				
 				done();
 			});	  
 		}); 
 		
 		it('.updateByID should return the number of the effected object', function(done){
-			appObject.name = "jeffrey sun";
-			mongostream.updateByID('user', appObject, function(err, num){
+            var newObject = {
+                id: appObject.id,
+                name: "jeffrey sun",
+                sex: "male"
+            };
+            
+			mongostream.updateByID('user', newObject, function(err, num){
 				should.not.exist(err);
 				should.exist(num);
 				num.should.equal(1);
@@ -66,8 +81,12 @@ describe('Mongo', function(){
 			mongostream.queryByID('user', appObject.id, function(err, obj){
 				should.not.exist(err);
 				should.exist(obj);
+               
+                obj.should.not.have.property('_id');
 				obj.should.have.property('id');
 				obj.should.have.property('name', 'jeffrey sun');	
+                obj.should.not.have.property('age'); 
+                obj.should.have.property('sex', 'male'); 
 				
 				done();
 			});	  
@@ -81,9 +100,9 @@ describe('Mongo', function(){
 			});	  
 		}); 
 		
-		it('.queryByID should return undefined when query the removed object', function(done){
+		it('.queryByID should return erro when the queried object does not exist', function(done){
 			mongostream.queryByID('user', appObject.id, function(err, obj){
-				should.not.exist(err);
+				should.exist(err);
 				should.not.exist(obj);
 				
 				done();
